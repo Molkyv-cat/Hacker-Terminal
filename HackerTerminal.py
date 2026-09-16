ed69d26ec865f36776702c89c4acbb9638317d9f
Molkyv <arsenii.pav0@gmail.com>
2026-09-16 19:25:40 +0300

    Add files via upload

---
+++ HackerTerminal.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+print("=== Login Terminal ===")
+
+name = input("Username: ")
+age = input("Age: ")
+
+print("Access Granted")
+
+timenow = datetime.now()
+
+print("Hello!")
+
+print("Your name:" , name)
+print("Your age:" , age)
+print("Time now:" , timenow.time())
+
+print("Have a nice day!")
